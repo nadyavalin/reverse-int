5e139112c4e32ee3a32577778f2ebbf01e07cfbe
nadyavalin <nadejda.sergeevna.tkachuk@gmial.com>
2023-08-08 15:25:31 +0300

    feat: do task reverse-int and add descriptions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    // преобразуем в строку, раделеяем, переворачиваем, соединяем
+    const reversedStr = String(n).split('').reverse().join('');
+    // используем функцию parseInt для преобразования результата в число с основанием 10 (десятичная система счисления)
+    return parseInt(reversedStr, 10);
 }
